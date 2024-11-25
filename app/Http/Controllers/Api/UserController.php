7d37b2d1914ca2b8550390dac4d7edf834c9ad96
OrenTwh <orenwh12@gmail.com>
2024-11-25 09:32:25 +0800

    api updates

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -36,8 +36,6 @@ class UserController extends Controller {
      * @bodyParam tmp_user string required The temporary user ID during request OTP. Example: eyJpdiI...
      * @bodyParam fullname string required The fullname for register. Example: John Wick
      * @bodyParam email string required The email for register. Example: johnwick@gmail.com
-     * @bodyParam calling_code string required The calling code for register. Example: +60
-     * @bodyParam phone_number integer required The phone number for register. Example: 123344212
      * @bodyParam otp string required The otp for register. Example: 123456
      * @bodyParam password string required The password for register. Example: abcd1234
      * @bodyParam password_confirmation string required The confirmation password. Example: abcd1234
