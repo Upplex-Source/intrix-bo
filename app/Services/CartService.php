b25351d67264268bff3fba5492102e9c01f5cee6
OrenTwh <orenwh12@gmail.com>
2025-03-19 01:54:57 +0800

    test quan

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -557,6 +557,9 @@ class CartService {
                 'product' => $meta->product->makeHidden( ['created_at','updated_at'.'status'] )->setAttribute('image_path', $meta->product->image_path),
                 'product_variant' => $meta->productVariant ? $meta->productVariant->makeHidden( ['created_at','updated_at'.'status'] )->setAttribute('image_path', $meta->productVariant->image_path) : null,
                 'quantity' => $meta->quantity,
+                'color' => $meta->productVariant ? $meta->productVariant->title : null,
+                'color_code' => $meta->productVariant ? $meta->productVariant->id : null,
+                'payment_plan' => $meta->payment_plan,
             ];
         });
 
