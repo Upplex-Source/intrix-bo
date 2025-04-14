0df996437bd8e83e9589dbeff054397992349d88
OrenTwh <orenwh12@gmail.com>
2025-04-14 20:41:35 +0800

    test

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -60,7 +60,7 @@ class CartService {
         $query = Cart::with(['cartMetas', 'addons', 'freeGift' => function ($query) {
                 $query->orderBy('created_at', 'DESC');
             }, 'voucher'])
-            ->whereIn('status', [ 21 ] )
+            ->whereIn('status', [ 21, 10 ] )
             ->orderBy('created_at', 'DESC');
     
         // Apply filters if 'id' or 'session_key' is provided
